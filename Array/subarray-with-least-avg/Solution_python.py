b03242ce266309bbd04a7dbd19a49ba152ce27df
Nitu <nitu.agarwal8@gmail.com>
2016-01-30 22:31:28 +0530

    updating python file for subarray question

--- Array/subarray-with-least-avg/Solution_python.py
+++ Array/subarray-with-least-avg/Solution_python.py
@@ -1,17 +1,19 @@
+def find_min_sub_array(arr,n,k):
+	if n<k:
+		return
+	curr_sum = sum(arr[:k]) # add first k items of array
+	min_sum = curr_sum
+	res_index=0
+	i=k
+	while i<n:
+		# Add current item arr[i] and remove first item of the sub array
+		curr_sum += arr[i] - arr[i-k]
+		if curr_sum< min_sum:
+			min_sum = curr_sum
+			res_index = i-k+1
+		i= i+1
+	print "the resultant index is ",(res_index,res_index+k-1)
+
 arr = [3, 7, 90, 20, 10, 50, 40]
 k = 3
-curr_sum = 0
-min_sum =0
-i=0
-res_index = 0
-while i<k:
-	curr_sum +=arr[i]
-	i = i+1
-min_sum = curr_sum
-while i <len(arr):
-	curr_sum += arr[i] - arr[i-k]
-	if curr_sum < min_sum:
-		min_sum = curr_sum
-		res_index = i-k+1
-	i = i+1
-print "the resultant index is ",(res_index,res_index+k-1)
+find_min_sub_array(arr,len(arr),k)
