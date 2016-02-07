6efb026144faf9201fb2b19a4ca89ff015ccb359
Mukesh <mksunda01@gmail.com>
2016-02-07 22:08:25 +0530

    Implemented Linkedlist in ruby

--- linked-list/linked-list.rb
+++ linked-list/linked-list.rb
@@ -1,7 +1,45 @@
 class Node
-  attr_accessor :node, :next
+  attr_accessor :value, :next
 
-  def initialize(node)
-    @node = node
+  def initialize(value)
+    @value = value
+  end
+end
+
+class LinkedList
+  attr_accessor :head, :tail
+
+  def initialize(head)
+    raise "LinkedList must be initialized with a Node." unless head.is_a?(Node)
+    @head = head
+    @tail = head
+  end
+
+  # Insert Node after the tail of the LinkedList.
+  def insert(node)
+    @tail.next = node
+    @tail = @tail.next
+  end
+
+  # Print out all the values of the LinkedList in order.
+  def print
+    current_node = @head
+
+    while current_node != nil
+      puts "\t#{current_node.value}"
+      current_node = current_node.next
+    end
+  end
+
+  # Iterate through LinkedList and yield values to block.
+  def each
+    return to_enum(:each) unless block_given?
+
+    current_node = @head
+
+    while current_node != nil
+      yield current_node.value
+      current_node = current_node.next
+    end
   end
 end
