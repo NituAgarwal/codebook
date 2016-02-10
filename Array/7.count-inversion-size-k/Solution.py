62a3d33df518173b0bd72c72ee04da165047acf9
Nitu <nitu.agarwal8@gmail.com>
2016-02-10 23:43:03 +0530

    Adding solution in n^2 for count inversion

--- Array/7.count-inversion-size-k/Solution.py
+++ Array/7.count-inversion-size-k/Solution.py
@@ -14,6 +14,25 @@ arr = [8,4,2,1]
 k=3
 count_inversion(arr,k)
 
+#Solution in O(n^2)
+def count_inversion_efficient(arr,k):
+	n =len(arr)
+	count =0
+	for i in range(1,n):
+		small = 0
+		for j in range(i+1,n):
+			if arr[j] < arr[i] :
+				small +=1
+		great =0
+		for j in range(i-1,-1,-1):
+			if arr[j] > arr[i]:
+				great +=1
+		count += small*great
+	print count
+
+arr = [8,4,2,1]
+k=3
+count_inversion_efficient(arr,k)
 
 
 
